21d8baccc8f5df65afaabda06dfe989e95220fec
Igor_MOD <142551791+IgorMDO@users.noreply.github.com>
2024-02-23 22:00:25 -0300

    Update basicpython.py

--- basicpython.py
+++ basicpython.py
@@ -7,4 +7,4 @@ n3 = input(f'Por fim, insira a última nota.\n')
 media = (int(n1) + int(n2) + int(n3)) / 3
 
 print(f'A média entre os valores: {n1}, {n2}, {n3} é igual a {media}')
-//obs: A variável nome, neste código é descartável, ou seja, há completa execução do código mesmo com sua retirada
+#obs: A variável nome, neste código é descartável, ou seja, há completa execução do código mesmo com sua retirada
